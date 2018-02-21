806ee73698079aa5ae07071c77e85acef745e8de
xaviertung <xaviertung@yeah.net>
2018-02-22 01:20:47 +0800

    add babel plugins

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 import '../css/index.less';
 document.write('Hello, World!');
+const obj1 = { a: 1 };
+const obj2 = { b: 2 };
+const obj3 = Object.assign({}, obj1, obj2);
+const a = obj3;
+document.write(JSON.stringify(obj3));
