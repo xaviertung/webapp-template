e3e029ef639a1b293dad5dc5a39f4c376737cab3
unknown <yue.dong@corp.bfd.com>
2018-07-21 17:58:29 +0800

    feat: add react, circle example

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,52 @@
 import '../css/index.less';
-document.write('Hello, World!');
-const obj1 = { a: 1 };
-const obj2 = { b: 2 };
-const obj3 = Object.assign({}, obj1, obj2);
-const a = obj3;
-document.write(JSON.stringify(obj3));
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+context.lineJoin = 'round';
+context.lineWidth = 30;
+
+context.font='24px Helvetica';
+context.fillText('Hello, World!', 175, 200);
+
+// context.strokeStyle='goldenrod';
+// context.strokeRect(75, 100, 200, 200);
+context.beginPath();
+context.strokeStyle='goldenrod';
+context.rect(75, 100, 200, 200);
+context.stroke();
+
+
+
+// context.fillStyle='rgba(0, 0, 255, 0.5)';
+// context.fillRect(325, 100, 200, 200);
+context.beginPath();
+context.fillStyle='rgba(0, 0, 255, 0.5)';
+context.rect(325, 100, 200, 200);
+context.fill();
+context.stroke();
+
+context.beginPath();
+context.strokeStyle='lightgreen';
+context.fillStyle='lightgreen';
+context.arc(200, 200, 100, 0, 5 / 6 * Math.PI, false);
+context.closePath();
+context.stroke();
+context.fill();
+
+
+
+
+
+context.canvas.onmousedown = e => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  
+}
+
+
+
+
+
+
+
+
